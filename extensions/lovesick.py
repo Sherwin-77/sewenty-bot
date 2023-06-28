bde085f72e6c2f815e953015414e08054b2bbcb1
sherwin-77 <sherwinak1@gmail.com>
2023-06-28 11:16:21 +0700

    Remove recruit feature

--- extensions/lovesick.py
+++ extensions/lovesick.py
@@ -20,24 +20,6 @@ logging.basicConfig(level=logging.INFO,
 logger = logging.getLogger("extension.lovesick")
 
 
-class SelectDropdown(Dropdown):
-    def __init__(self, text: str, select_list: List[discord.SelectOption], recruit_log):
-        self.recruit_log = recruit_log
-        super().__init__(text, select_list)
-
-    async def callback(self, interaction: discord.Interaction):
-        assert self.view is not None
-        if interaction.user.id != self.view.ctx.author.id:
-            return await interaction.response.send_message("You are not allowed to do this :c", ephemeral=True)
-        await self.view.change_source(paginators.EmbedSource(self.recruit_log[self.values[0]] or ["None here"], 10))
-        for x in self.options:
-            if x.value == self.values[0]:
-                x.default = True
-            else:
-                x.default = False
-        await interaction.response.edit_message(view=self.view)
-
-
 # TODO: Move this to view_util, Fix inconsistent self var
 class QueryDropdown(Dropdown):
     def __init__(self, text: str, select_list: List[discord.SelectOption]):
@@ -163,14 +145,7 @@ class LoveSick(commands.Cog):
     def __init__(self, bot: SewentyBot):
         self.bot: SewentyBot = bot
         self.LXV_COLLECTION = self.bot.DB["lovesick"]
-        self.BLUEPRINT = {
-            "added": [],
-            "expired": [],
-            "deleted": []
-        }
 
-        self.logging_channel_id = 0
-        self.lxv_recruit_id = 0
         self.lxv_member_id = 0
         self.lxv_link_channel = 0
         self.event_link_channel = 0
@@ -181,7 +156,6 @@ class LoveSick(commands.Cog):
         self.ignored = set()
         self.verified = set()
         self.last_checked = None
-        self.recruit_log = deepcopy(self.BLUEPRINT)
 
     def cog_check(self, ctx) -> bool:
         return ctx.guild.id == self.GUILD_ID
@@ -192,8 +166,6 @@ class LoveSick(commands.Cog):
             logger.error("No setting for lovesick found. Unloading cog...")
             await self.bot.remove_cog("extensions.lovesick")
         # Note that id always stored in str due to big number
-        self.logging_channel_id = int(setting["logging_channel_id"])
-        self.lxv_recruit_id = int(setting["lxv_recruit_id"])
         self.lxv_member_id = int(setting["lxv_member_id"])
         self.lxv_link_channel = int(setting["lxv_link_channel"])
         self.event_link_channel = int(setting["event_link_channel"])
@@ -205,61 +177,51 @@ class LoveSick(commands.Cog):
         self.event_disabled = setting["event_disabled"]
         self.focus = setting["focus"]
         self.focus.sort()
-        self.refresh_lxv_recruit.start()
+        self.ping_lxv_db.start()
 
     async def cog_unload(self) -> None:
-        self.refresh_lxv_recruit.cancel()
+        self.ping_lxv_db.cancel()
 
     @tasks.loop(hours=12)
-    async def refresh_lxv_recruit(self):
-        if self.bot.TEST_MODE:
-            return
+    async def ping_lxv_db(self):
+        stats = self.LXV_COLLECTION.aggregate([{"$collStats": {"latencyStats": {"histograms": False}}}])
+        read_latency = []
+        total_read = 0
+        write_latency = []
+        total_write = 0
+        command_latency = []
+        total_command = 0
+        transaction_latency = []
+        total_transaction = 0
+        dt = 0
+        async for x in stats:
+            rd = x["latencyStats"]["reads"]
+            read_latency.append(rd["latency"])
+            total_read += rd["ops"]
+            wr = x["latencyStats"]["writes"]
+            write_latency.append(wr["latency"])
+            total_write += wr["ops"]
+            cmd = x["latencyStats"]["commands"]
+            command_latency.append(cmd["latency"])
+            total_command += cmd["ops"]
+            ts = x["latencyStats"]["transactions"]
+            transaction_latency.append(ts["latency"])
+            total_transaction += ts["ops"]
+            dt = x["localTime"]
         guild = self.bot.get_guild(self.GUILD_ID)
-        role = guild.get_role(self.lxv_recruit_id)
-        channel = guild.get_channel(self.logging_channel_id)
-        if role is None:
-            if channel is None:
-                return await self.bot.send_owner(f"Logging channel for lxv is missing. Old: {self.logging_channel_id}\n"
-                                                 f"LXV recruit role missing. Old: {self.lxv_recruit_id}")
-            else:
-                await channel.send(f"LXV recruit role missing. Old id: {self.lxv_recruit_id}")
-                return await self.bot.send_owner(f"LXV recruit role missing. Old: {self.lxv_recruit_id}")
-
-        current_date = datetime.datetime.utcnow()
-        count = await self.LXV_COLLECTION.count_documents({"_id": "lxvrecruit"})
-        old_data = await self.LXV_COLLECTION.find_one({"_id": "lxvrecruit"})
-
-        if not count:
-            old_data = dict()
-        else:
-            old_data = old_data["members"]
-        data = dict()
-        self.recruit_log = deepcopy(self.BLUEPRINT)
-        for member in role.members:
-            member_id = f"user{member.id}"
-            if member_id not in old_data:
-                self.recruit_log["added"].append(f"New recruit detected: {member} ({member.id})")
-                data.update({member_id: current_date})
-                continue
-            if member_id in old_data and (current_date - old_data[member_id]).days > 33:
-                self.recruit_log["expired"].append(f"{member} ({member.id})\n "
-                                                   f"From {old_data[member_id].strftime('%Y/%m/%d')} "
-                                                   f"(**{(current_date - old_data[member_id]).days}** days)")
-            data.update({member_id: old_data[member_id]})
-
-        self.recruit_log["deleted"] = [f"Deleted recruit role id: {x}" for x in old_data if x not in data]
-        self.last_checked = current_date
-
-        if not count:
-            await self.LXV_COLLECTION.insert_one({"_id": "lxvrecruit", "members": data})
-        else:
-            await self.LXV_COLLECTION.update_one({"_id": "lxvrecruit"}, {"$set": {"members": data}})
-        if channel is None:
-            return await self.bot.send_owner(f"Logging channel for lxv is missing. Old: {self.logging_channel_id}")
         ch = guild.get_channel(765818685922213948)
-        await ch.send("Update data completed")
+        await ch.send(f"# Data reporting\n"
+                      f"Read Latency: Average **{sum(read_latency)//(len(read_latency)*1000)} ms** "
+                      f"in {total_read} operations\n"
+                      f"Write Latency: Average **{sum(write_latency)//(len(write_latency)*1000)} ms** "
+                      f"in {total_write} operations\n"
+                      f"Command Latency: Average **{sum(command_latency)//(len(command_latency)*1000)} ms** "
+                      f"in {total_command} operations\n"
+                      f"Transaction Latency: Average **{sum(transaction_latency)//(len(transaction_latency)*1000)} ms**"
+                      f" in {total_transaction} operations\n"
+                      f"{discord.utils.format_dt(dt, 'F')}")
 
-    @refresh_lxv_recruit.before_loop
+    @ping_lxv_db.before_loop
     async def check_connected(self):
         logger.info("Waiting for bot...")
         await self.bot.wait_until_ready()
@@ -410,186 +372,6 @@ class LoveSick(commands.Cog):
                 await self.LXV_COLLECTION.update_one({"_id": "verified_msg"},
                                                      {"$set": {"msg_ids": list(self.verified)}})
             await message.reply(f"Sent to {link_channel.mention}")
-
-    @commands.command(aliases=["lxvlr"])
-    async def listrecruit(self, ctx):
-        """
-        Show list of all recruits
-        """
-        guild = self.bot.get_guild(self.GUILD_ID)
-        role = guild.get_role(self.lxv_recruit_id)
-        list_member = [f"{member.display_name} ({member.id})" for member in role.members]
-        pages = paginators.SimplePages(source=paginators.EmbedSource(list_member, 10))
-        await pages.start(ctx)
-
-    @commands.command(aliases=["lxvrl"])
-    async def recruitlog(self, ctx):
-        """
-        Core command of shidu
-        added roles show a list of added recruits in past 12 hours
-        deleted roles show a list of deleted recruits in past 12 hours
-        expired roles show a list of recruits that exceed 30 days
-        """
-        dropdown = SelectDropdown(text="Select log type",
-                                  select_list=[discord.SelectOption(label=key, value=value) for value, key in
-                                               {
-                                                   "added": "Added role",
-                                                   "expired": "Expired role",
-                                                   "deleted": "Deleted role"
-                                               }.items()],
-                                  recruit_log=self.recruit_log)
-        dropdown.options[1].default = True
-        pages = paginators.SimplePages(source=paginators.EmbedSource(self.recruit_log["expired"] or ["None here"], 10))
-        pages.add_item(dropdown)
-        await pages.start(ctx)
-
-    @commands.command(aliases=["lxvrc"])
-    async def recruitcheck(self, ctx, member: discord.Member):
-        """
-        Check recruit status of member
-        """
-        role = discord.utils.get(member.roles, id=self.lxv_recruit_id)
-        count = await self.LXV_COLLECTION.count_documents({"_id": "lxvrecruit"})
-        old_data = await self.LXV_COLLECTION.find_one({"_id": "lxvrecruit"})
-        data_id = f"user{member.id}"
-        current_date = datetime.datetime.utcnow()
-        if not count:
-            await ctx.send("Empty database. Please initialize with s!forcerefresh")
-        old_data = old_data["members"]
-
-        if role is None and data_id not in old_data:
-            return await ctx.send("User doesn't have role here neither in database")
-        if role is not None and data_id not in old_data:
-            return await ctx.send("User have the role but doesn't exist in database. "
-                                  "For staff please do s!forcerefresh")
-        if role is None and data_id in old_data:
-            await ctx.send("User doesn't have role but exist in database. For staff consider doing s!forcerefresh")
-        custom_embed = discord.Embed(title="Recruit info",
-                                     description=f"Recruit since: {old_data[data_id]} "
-                                                 f"({(current_date - old_data[data_id]).days} days)\n"
-                                                 f"Other format:\n"
-                                                 f"{discord.utils.format_dt(old_data[data_id], 'T')}\n"
-                                                 f"{discord.utils.format_dt(old_data[data_id], 'd')}\n"
-                                                 f"{discord.utils.format_dt(old_data[data_id], 'D')}\n"
-                                                 f"{discord.utils.format_dt(old_data[data_id], 'f')}\n"
-                                                 f"{discord.utils.format_dt(old_data[data_id], 'F')}\n"
-                                                 f"{discord.utils.format_dt(old_data[data_id], 'R')}\n"
-                                                 f"Nothing to show again? Just do s!whois",
-                                     color=discord.Colour.random())
-        await ctx.send(embed=custom_embed)
-
-    @commands.command(aliases=["lxvfr"])
-    async def forcerefresh(self, ctx: commands.Context):
-        """
-        Forcefully refreshes recruit list
-        """
-        if not self.mod_only(ctx):
-            return await ctx.send("You are not allowed to use this command >:(")
-        role = ctx.guild.get_role(self.lxv_recruit_id)
-        current_date = datetime.datetime.utcnow()
-        count = await self.LXV_COLLECTION.count_documents({"_id": "lxvrecruit"})
-        old_data = await self.LXV_COLLECTION.find_one({"_id": "lxvrecruit"})
-
-        if not count:
-            old_data = dict()
-        else:
-            old_data = old_data["members"]
-        data = dict()
-        self.recruit_log = deepcopy(self.BLUEPRINT)
-        for member in role.members:
-            member_id = f"user{member.id}"
-            if member_id not in old_data:
-                self.recruit_log["added"].append(f"New recruit detected: {member} ({member.id})")
-                data.update({member_id: current_date})
-                continue
-            if member_id in old_data and (current_date - old_data[member_id]).days > 33:
-                self.recruit_log["expired"].append(f"{member} ({member.id})\n "
-                                                   f"From {old_data[member_id].strftime('%Y/%m/%d')} "
-                                                   f"(**{(current_date - old_data[member_id]).days}** days)")
-            data.update({member_id: old_data[member_id]})
-
-        self.recruit_log["deleted"] = [f"Deleted recruit role id: {x}" for x in old_data if x not in data]
-        self.last_checked = current_date
-
-        if not count:
-            await self.LXV_COLLECTION.insert_one({"_id": "lxvrecruit", "members": data})
-        else:
-            await self.LXV_COLLECTION.update_one({"_id": "lxvrecruit"}, {"$set": {"members": data}})
-
-        await ctx.send("Successfully force refreshed")
-
-    @commands.command()
-    async def lxv(self, ctx):
-        custom_embed = discord.Embed(title="This looks cool",
-                                     description=f"Last updated: {self.last_checked}",
-                                     color=discord.Colour.random())
-        await ctx.send(embed=custom_embed)
-
-    @commands.command(aliases=["lxvafr"])
-    async def accuraterefresh(self, ctx, limit=100):
-        """
-        Accurete refresh for recruits date by checking audit log
-        """
-        if limit > 1000:
-            return await ctx.send("Too big")
-        if not self.mod_only(ctx):
-            return await ctx.send("You are not allowed to use this command >:(")
-        original = await ctx.send("Please wait <a:discordloading:792012369168957450>")
-        count = await self.LXV_COLLECTION.count_documents({"_id": "lxvrecruit"})
-        old_data = await self.LXV_COLLECTION.find_one({"_id": "lxvrecruit"})
-        channel = ctx.guild.get_channel(self.logging_channel_id)
-
-        if not count:
-            old_data = dict()
-        else:
-            old_data = old_data["members"]
-        new_data = dict()
-        async for x in ctx.guild.audit_logs(limit=limit, action=discord.AuditLogAction.member_role_update):
-            tmp_id = f"user{x.target.id}"
-            if tmp_id in new_data:
-                continue
-            check_date = None
-            for y in x.after.roles:
-                if y.id == self.lxv_recruit_id:
-                    check_date = x.created_at
-            if check_date is None:
-                continue
-            new_data.update({tmp_id: check_date})
-        merged_data = {**old_data, **new_data}
-        if not count:
-            await self.LXV_COLLECTION.insert_one({"_id": "lxvrecruit", "members": merged_data})
-        else:
-            await self.LXV_COLLECTION.update_one({"_id": "lxvrecruit"}, {"$set": {"members": merged_data}})
-        await original.edit(content="Done <:wurk:858721776770744320>")
-        await channel.send(f"{ctx.author} used accurate refresh command with limit of last {limit} audit log")
-
-    @commands.command()
-    async def lxvrefresh(self, ctx):
-        if not self.mod_only(ctx):
-            return await ctx.send("Huh")
-        original = await ctx.send("Refreshing <a:discordloading:792012369168957450>")
-        setting = await self.LXV_COLLECTION.find_one({"_id": "setting"})
-        if not setting:
-            return await ctx.send("ERROR: Setting doen't exist at database")
-        self.logging_channel_id = int(setting["logging_channel_id"])
-        self.lxv_recruit_id = int(setting["lxv_recruit_id"])
-        self.lxv_member_id = int(setting["lxv_member_id"])
-        self.lxv_link_channel = int(setting["lxv_link_channel"])
-        self.event_link_channel = int(setting["event_link_channel"])
-        self.lxv_only_event = setting["lxv_only_event"]
-        self.mod_ids = set(map(int, setting["mod_ids"]))
-        verified = await self.LXV_COLLECTION.find_one({"_id": "verified_msg"})
-        if verified:
-            self.verified = set(map(int, verified["msg_ids"]))
-        self.event_disabled = setting["event_disabled"]
-        self.focus = setting["focus"]
-        self.focus.sort()
-        await original.edit(content="Done <:wurk:858721776770744320>")
-
-    @commands.command(hidden=True)
-    @commands.is_owner()
-    async def lxveval(self, ctx, var):
-        return await ctx.send(eval(var))
 
     @commands.group(invoke_without_command=True, aliases=["ev"])
     async def event(self, ctx):
