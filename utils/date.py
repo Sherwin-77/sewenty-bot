25d7223056cd4f1be2072a0661a1b83f136d8d19
Sherwin Prasetia Makmur <sherwin.prasetia.makmur@springkraf.com>
2024-10-01 16:22:09 +0700

    feat(util): Add more date util

--- utils/date.py
+++ utils/date.py
@@ -9,3 +9,10 @@ def add_months(date: datetime.datetime, months: int, reset_day: Optional[int] = 
     day = min(reset_day or date.day, calendar.monthrange(year, month)[1])
 
     return date.replace(year=year, month=month, day=day)
+
+
+def start_of_day(date: datetime.datetime) -> datetime.datetime:
+    return date.replace(hour=0, minute=0, second=0, microsecond=0)
+
+def end_of_day(date: datetime.datetime) -> datetime.datetime:
+    return date.replace(hour=23, minute=59, second=59, microsecond=999999)
